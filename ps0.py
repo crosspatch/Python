eba035666ca19112e2eec22315914797abcf2590
crosspatch <christine.d.lynch@gmail.com>
2019-01-10 12:27:16 -0500

    MIT's Problem Set 1

---
+++ ps0.py
@@ -0,0 +1,11 @@
+# Problem Set #1: Powers and Logarithms using Numpy
+
+#Import Numpy for math functions
+import numpy as numpy
+
+#Store numbers as floats
+x = int(input("Enter number x: "))
+y = int(input("Enter number y: "))
+#Print results to screen
+print("X**y =  " + str(numpy.power(x,y)))
+print("log(x) = " + str(int(numpy.log2(x))))
